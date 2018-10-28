9b2346d87ebc5fd72c01eb84a58a50447ff7a469
softwase <softwase@gmail.com>
2018-10-28 12:43:56 +0200

    add menu component with bootstrap

---
+++ bookstore/src/components/Menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Menu = () => (
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Expand at lg</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarsExample05">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Menu;
