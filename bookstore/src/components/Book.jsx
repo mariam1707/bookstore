2c8a974bfd7b6f003a55fbbb346bf9689757706d
softwase <softwase@gmail.com>
2018-10-28 22:20:29 +0200

    trying to make a modal window for editing

--- bookstore/src/components/Book.jsx
+++ bookstore/src/components/Book.jsx
@@ -4,7 +4,7 @@ import ModalEdit from './ModalEdit'
 
 const Book = ({ book }) => (
     <div className="card-wrap col-md-4 ">
-        {console.log(book)}
+        {/* {console.log(book)} */}
         <div className="card mb-4 box-shadow">
             <img className="card-img-top" src={book.image} alt="Card image cap"></img>
             <div className="card-body">
@@ -13,7 +13,7 @@ const Book = ({ book }) => (
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                         <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <ModalEdit />
+                        <ModalEdit book={book} />
                     </div>
                     <small className="text-muted">9 mins</small>
                 </div>
