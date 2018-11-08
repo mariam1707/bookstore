17ae9c224c87057254766890cf10e976ec8b3389
softwase <softwase@gmail.com>
2018-11-08 15:34:21 +0200

    create saga for auth actions

--- bookstore.v2/client/src/saga/authSaga.js
+++ bookstore.v2/client/src/saga/authSaga.js
@@ -1,13 +1,40 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
 import axios from 'axios';
 
-import { SUBMIT_LOGIN_SAGA } from '../actions/auth';
+import { SUBMIT_LOGIN_SAGA, authErrors, SUBMIT_REGISTRATION_SAGA } from '../actions/auth';
 
-export function* getLogin() {
+export function* makeAuth({ payload }) {
+  const opt = {
+    url: 'api/users/register',
+    method: 'post',
+    data: payload,
+  };
   try {
-  } catch (error) {}
+    const response = yield call(axios, opt);
+    console.log(response);
+  } catch (error) {
+    const message = error.response.data;
+    yield put(authErrors(message));
+  }
+}
+
+export function* getLogin({ payload }) {
+  const opt = {
+    url: 'api/users/login',
+    method: 'post',
+    data: payload,
+  };
+  try {
+    const response = yield call(axios, opt);
+    console.log(response);
+  } catch (error) {
+    const message = error.response.data;
+    yield put(authErrors(message));
+  }
 }
 
 export default function*() {
+  // yield takeEvery(SUBMIT_LOGIN_SAGA, getLogin);
+  yield takeEvery(SUBMIT_REGISTRATION_SAGA, makeAuth);
   yield takeEvery(SUBMIT_LOGIN_SAGA, getLogin);
 }
--- bookstore.v2/client/src/saga/sagas.js
+++ bookstore.v2/client/src/saga/sagas.js
@@ -1,6 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import bookSaga from './bookSaga';
+import authSaga from './authSaga';
 
 export default function* rootSaga() {
   yield fork(bookSaga);
+  yield fork(authSaga);
 }
