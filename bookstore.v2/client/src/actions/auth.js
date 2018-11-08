da7c8f80e711e7ba8a839d011a6a7677828718e8
softwase <softwase@gmail.com>
2018-11-08 15:34:52 +0200

    add errors actions

--- bookstore.v2/client/src/actions/auth.js
+++ bookstore.v2/client/src/actions/auth.js
@@ -3,7 +3,11 @@ import { createAction } from 'redux-actions';
 export const SUBMIT_LOGIN = 'SUBMIT_LOGIN';
 export const SUBMIT_LOGIN_SAGA = 'SUBMIT_LOGIN_SAGA';
 export const SUBMIT_REGISTRATION_SAGA = 'SUBMIT_REGISTRATION_SAGA';
+export const SUBMIT_REGISTRATION = 'SUBMIT_REGISTRATION';
+export const AUTH_ERRORS = 'AUTH_ERRORS';
 
 export const submitLogin = createAction(SUBMIT_LOGIN);
 export const submitLoginSaga = createAction(SUBMIT_LOGIN_SAGA);
 export const submitRegistrationSaga = createAction(SUBMIT_REGISTRATION_SAGA);
+export const submitRegistration = createAction(SUBMIT_REGISTRATION);
+export const authErrors = createAction(AUTH_ERRORS);
