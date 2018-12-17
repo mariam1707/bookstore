0f4a0342ae7fed9b6c177db980d0100dcf5e1d6e
softwase <softwase@gmail.com>
2018-12-17 17:23:13 +0200

    react intl update

--- client/src/containers/App/index.jsx
+++ client/src/containers/App/index.jsx
@@ -2,18 +2,18 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router';
-import { ConnectedRouter } from 'connected-react-router';
+// import { Route, Switch } from 'react-router';
+// import { ConnectedRouter } from 'connected-react-router';
 import 'bootstrap';
 
-import Login from 'containers/Login';
-import Registration from 'components/Registration';
-import RestorePassword from 'containers/RestorePassword';
-// import Content from 'containers/Content';
-import BooksWrap from 'containers/BooksWrap';
-import SingleBookCreateView from 'containers/SingleBookCreateView';
+// import Login from 'containers/Login';
+// import Registration from 'components/Registration';
+// import RestorePassword from 'containers/RestorePassword';
+// // // import Content from 'containers/Content';
+// import BooksWrap from 'containers/BooksWrap';
+// import SingleBookCreateView from 'containers/SingleBookCreateView';
 import LanguageProvider from 'components/LanguageProvider';
-import history from '../../helpers/history';
+// import history from '../../helpers/history';
 import store from '../../store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,17 +22,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 const App = () => (
   <Provider store={store}>
-    <LanguageProvider>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path="/" component={BooksWrap} />
-          <Route path="/login" component={Login} />
-          <Route path="/registration" component={Registration} />
-          <Route path="/restore" component={RestorePassword} />
-          <Route path="/create-book" component={SingleBookCreateView} />
-        </Switch>
-      </ConnectedRouter>
-    </LanguageProvider>
+    <LanguageProvider />
   </Provider>
 );
 
